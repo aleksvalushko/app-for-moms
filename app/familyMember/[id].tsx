8141a6673f4484b6c13f763ebfc222f9b86179c5
aleks.valushko <aleks.valushko@yandex.ru>
2025-12-11 17:27:48 +0300

    modify family-member component

--- app/familyMember/[id].tsx
+++ app/familyMember/[id].tsx
@@ -1,37 +1,85 @@
-import {router, useLocalSearchParams} from 'expo-router'
-import {SafeAreaView} from "react-native-safe-area-context";
+import {router} from 'expo-router'
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import CustomText from "@/components/CustomText";
 import {COLORS} from "@/constants/colors";
 import CustomTouchableHighlight from "@/components/Buttons/CustomTouchableHighlight";
 import React from "react";
 import {useColorScheme} from "nativewind";
 import {useUser} from "@/hooks/useUser";
+import {FlatList, StyleSheet, View} from "react-native";
+import {useAppSelector} from "@/hooks";
+import GoBackButton from "@/components/Buttons/GoBackButton";
+
+type FamilyMemberActionsType = {
+    id: number;
+    title: string;
+    icon?: string;
+    onPress: () => void;
+};
 
 export default function FamilyMember() {
-    const {id} = useLocalSearchParams();
     const {colorScheme} = useColorScheme();
     const {user} = useUser();
 
-    const goBack = () => {
-        if (router.canGoBack()) {
-            router.back();
-            return;
-        }
-        if (user !== null) {
-            router.replace("/(tabs)");
-            return;
-        }
-        router.replace("/(auth)");
-    };
+    const familyMember = useAppSelector(state => {
+        return state.familyMembers.familyMember;
+    });
+
+    const familyMemberActions: FamilyMemberActionsType[] = [
+        {
+            id: 1, title: 'Календарные события', icon: 'calendar', onPress: goToCalendar
+        },
+        {id: 2, title: 'Школа', icon: 'school', onPress: () => {}},
+        {id: 3, title: 'Тренировки', icon: 'gymnastics', onPress: () => {}},
+    ];
+
+    function goToCalendar() {
+        router.navigate('/calendar/[id]');
+    }
+
+    const Item = ({item}: { item: FamilyMemberActionsType }) => (
+        <CustomTouchableHighlight pressFunction={item.onPress} name={item.title} style={styles.item} underlayColor={COLORS[colorScheme].colors.underlayForListElement}
+                                  className='min-h-[50px] flex-row items-center justify-start bg-listElement my-[5px] max-w-full'
+                                  textClassName='text-white text-[20px]'
+                                  iconName={item.icon} isLeftIcon={true}/>
+    );
 
     return (
         <SafeAreaView className='flex-1 justify-center items-center p-[10px]'>
-            <CustomText className='flex-1 flex w-full h-full items-center justify-center text-black dark:text-white'> Family member id: {id}</CustomText>
-            <CustomTouchableHighlight pressFunction={goBack} name='Вернуться назад'
-                                      underlayColor={COLORS[colorScheme].colors.underlayForListElement}
-                                      className='flex-5 max-h-[50px] bg-listElement w-full'
-                                      textClassName='text-white text-[20px]'
-                                      iconName='arrow-left' isLeftIcon={true}/>
+            <View className='w-full flex-row justify-center items-center relative my-[10px]'>
+                <GoBackButton user={user} />
+                <CustomText className='text-[30px] text-black dark:text-white'>{familyMember?.name ?? ''}</CustomText>
+            </View>
+            <SafeAreaProvider style={styles.provider}>
+                <SafeAreaView style={styles.container}>
+                    <FlatList<FamilyMemberActionsType>
+                        data={familyMemberActions}
+                        renderItem={({item}: { item: FamilyMemberActionsType }) => <Item item={item}/>}
+                        keyExtractor={(item: FamilyMemberActionsType) => item.id.toString()}
+                    />
+                </SafeAreaView>
+            </SafeAreaProvider>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    provider: {
+        width: '100%',
+        columnGap: 10,
+    },
+    container: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
+    item: {
+        flex: 1,
+        padding: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+});
