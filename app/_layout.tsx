499fc64cb25378cf490551e358d7877ec978dc04
aleks.valushko <aleks.valushko@yandex.ru>
2025-12-11 17:27:28 +0300

    add new stack screen for calendar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout() {
                         <Stack.Screen name="(auth)" options={{headerShown: false}}/>
                         <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
                         <Stack.Screen name="familyMember/[id]" options={{headerShown: false}}/>
+                        <Stack.Screen name="calendar/[id]" options={{headerShown: false}}/>
                     </Stack>
                 </ThemeProvider>
             </UserProvider>
