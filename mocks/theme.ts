f05b641444e6ca05a5998ef4eea14649f678bda3
aleks.valushko <aleks.valushko@yandex.ru>
2025-12-31 12:51:00 +0300

    modify main colors and colors for calendar

--- mocks/theme.ts
+++ mocks/theme.ts
@@ -1,30 +1,32 @@
 import {COLORS} from "@/constants/colors";
-import {useColorScheme} from "nativewind";
 
 export const darkThemeColor = 'rgb(71,71,71)';
 
-export function getTheme() {
-    const {colorScheme} = useColorScheme();
-
+export function getTheme(colorScheme: 'light' | 'dark' | null | undefined) {
     const isDarkMode = colorScheme === 'dark';
     const disabledColor = 'grey';
 
     return {
-        expandableKnobColor: darkThemeColor,
-        disabledDotColor: disabledColor,
-        textDayStyle: {marginTop: 1},
-        dotStyle: {marginTop: -2},
-        backgroundColor: isDarkMode ? COLORS[colorScheme].colors.background : COLORS[colorScheme].colors.background,
-        calendarBackground: isDarkMode ? COLORS[colorScheme].colors.background : COLORS[colorScheme].colors.background,
+        backgroundColor: isDarkMode
+            ? COLORS[colorScheme].colors.background
+            : COLORS[colorScheme].colors.background,
+        calendarBackground: isDarkMode
+            ? COLORS[colorScheme].colors.background
+            : COLORS[colorScheme].colors.background,
+        textDayStyle: { marginTop: 1 },
         textSectionTitleColor: '#b6c1cd',
         textSectionTitleDisabledColor: '#d9e1e8',
         selectedDayBackgroundColor: '#00adf5',
         selectedDayTextColor: '#ffffff',
         todayTextColor: '#00adf5',
-        dayTextColor: '#2d4150',
-        textDisabledColor: '#d9e1e8',
+        dayTextColor: isDarkMode
+            ? COLORS[colorScheme].colors.text
+            : COLORS[colorScheme].colors.text,
+        textDisabledColor: disabledColor,
         dotColor: '#00adf5',
         selectedDotColor: '#ffffff',
+        disabledDotColor: disabledColor,
+        dotStyle: { marginTop: -2 },
         arrowColor: '#00adf5',
         disabledArrowColor: '#d9e1e8',
         monthTextColor: 'blue',
@@ -37,6 +39,6 @@ export function getTheme() {
         textDayHeaderFontWeight: '300',
         textDayFontSize: 20,
         textMonthFontSize: 25,
-        textDayHeaderFontSize: 16
+        textDayHeaderFontSize: 16,
     };
 }
